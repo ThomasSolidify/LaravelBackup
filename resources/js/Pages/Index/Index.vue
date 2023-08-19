551561d59bbb7303c507513fceceb2665dc6d3fe
Thomas Johnson <53288963+ThomasSolidify@users.noreply.github.com>
2023-08-20 00:16:12 +0100

    Your commit message here

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      Show
+      <InertiaLink href="/hello">Hello Page</InertiaLink>
+    </div>
+  </template>
+
+  <script setup>
+  import { Link as InertiaLink } from '@inertiajs/inertia-vue3'
+  </script>
