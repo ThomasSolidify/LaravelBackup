0886ded79764a14c8254e7ddfe9cb1b0457c3c81
Thomas Johnson <53288963+ThomasSolidify@users.noreply.github.com>
2023-08-20 00:23:31 +0100

    gds

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
+  resolve: async (name) => {
+    const pages = import.meta.glob('./Pages/**/*.vue')
+
+    return (await pages[`./Pages/${name}.vue`]())
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
